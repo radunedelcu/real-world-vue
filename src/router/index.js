8bc35dd569cb37fd0a3698cce431e308b648b287
radunedelcu <94298902+radunedelcu@users.noreply.github.com>
2022-09-22 09:02:26 +0300

    Add routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,72 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
-import EventDetails from "../views/EventDetails.vue"
-import AboutView from "../views/AboutView.vue";
+import { createRouter, createWebHistory } from "vue-router"
+import EventList from "../views/EventList.vue"
+import EventDetails from "../views/event/EventDetails.vue"
+import EventLayout from "../views/event/EventLayout.vue"
+import EventRegister from "../views/event/EventRegister.vue"
+import EventEdit from "../views/event/EventEdit.vue"
+import AboutView from "../views/AboutView.vue"
+import NotFound from '../views/NotFound.vue'
+import NetworkError from '../views/NetworkError.vue'
 
 const routes = [
   {
     path: "/",
     name: "EventList",
-    component: EventList
+    component: EventList,
+    props: route => ({page: parseInt(route.query.page) || 1})
+  },
+  
+  {
+    path: "/events/:id",
+    name: "EventLayout",
+    props: true,
+    component: EventLayout,
+    children:[
+      {
+        path: "",
+    name: "EventDetails",
+    component: EventDetails
+      },
+      {
+        path: "register",
+        name: "EventRegister",
+       
+        component: EventRegister
+      },
+      {
+        path: "edit",
+        name: "EventEdit",
+      
+        component: EventEdit
+      },
+    ]
   },
   {
-    path: "/event/:id",
-    name: "EventDetails",
-    props: true,
-    component: EventDetails
+    path:'/event/:afterEvent(.*)',
+    redirect: to => {
+      return {path: '/events/' + to.params.afterEvent}
+    }
   },
   {
     path: "/about",
     name: "about",
     component: AboutView
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFound,
+    props: true
+  },
+  {
+    path: '/network-error',
+    name: 'NetworkError',
+    component: NetworkError
   }
 ];
 
